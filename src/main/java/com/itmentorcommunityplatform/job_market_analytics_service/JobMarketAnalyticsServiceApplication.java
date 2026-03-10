d2a5c4d64c4312f224ce40f475839be7c676eee5
Misha Pivovarov <pivovarov.pr@gmail.com>
2026-03-10 14:50:34 +0300

    feat: initialize job-market-analytics-service

---
+++ src/main/java/com/itmentorcommunityplatform/job_market_analytics_service/JobMarketAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itmentorcommunityplatform.job_market_analytics_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMarketAnalyticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMarketAnalyticsServiceApplication.class, args);
+	}
+
+}
