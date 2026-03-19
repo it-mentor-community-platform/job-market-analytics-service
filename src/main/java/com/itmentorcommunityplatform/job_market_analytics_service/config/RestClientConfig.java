c588f58ea5771d9d69b5f97aa98ba76a8c609020
Misha Pivovarov <pivovarov.pr@gmail.com>
2026-03-19 20:40:38 +0300

    feat: добавлена конфигурация HH API и rate limit

---
+++ src/main/java/com/itmentorcommunityplatform/job_market_analytics_service/config/RestClientConfig.java
@@ -0,0 +1,17 @@
+package com.itmentorcommunityplatform.job_market_analytics_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestClient restClient(@Value("${hh.api.url}") String baseUrl) {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+}
