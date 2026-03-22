d5b30b3eb9cef8d469fcfcf9738b7ede9cbbfcf6
Misha Pivovarov <pivovarov.pr@gmail.com>
2026-03-22 15:35:31 +0300

    fix: поменял название Bean на более точный

--- src/main/java/com/itmentorcommunityplatform/job_market_analytics_service/config/RestClientConfig.java
+++ src/main/java/com/itmentorcommunityplatform/job_market_analytics_service/config/RestClientConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient(@Value("${hh.api.url}") String baseUrl) {
+    public RestClient hhRestClient(@Value("${hh.api.url}") String baseUrl) {
         return RestClient.builder()
                 .baseUrl(baseUrl)
                 .build();
